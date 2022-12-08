76782376fe02df3f10b25bb01ea1ed4af4ad5399
McBain <cwma.campbell@gmail.com>
2022-12-08 14:53:01 +0000

    added alternate version

--- readdir.pl
+++ readdir.pl
@@ -5,3 +5,35 @@ opendir (DIR, $dir);
      @dir=readdir(DIR);
         closedir(DIR);
          @dir = sort { -M "$dir/$a" <=> -M "$dir/$b" } (@dir);
+         
+## or ##
+
+# Get a list of the files
+opendir(DIR, $DirName);
+@Files1 = readdir(DIR);
+closedir(DIR);
+
+# Loop thru each of these files
+foreach $File (@Files1) {
+	
+	# Get information (including last modified date) about file
+	@FileData = stat($DirName."/".$File);
+	
+	# Push this into a new array with date at front
+	push(@Files, @FileData[9]."&&".$File);
+	
+}
+
+# Sort this array
+@Files = reverse(sort(@Files));
+
+# Loop thru the files
+foreach $File (@Files) {
+	
+	# Get the filename back from the string
+	($Date,$FileName) = split(/\\&\\&/,$File);
+	
+	# Print the filename
+	print "$FileName<BR>";
+	
+}
